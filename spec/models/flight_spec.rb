c0e1f1ed2474ece4f2a0166a1addfdee4d48049d
DeMarcus Kirby <tkd024@gmail.com>
2020-01-13 11:33:28 -0700

    Add test for model methods

--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -13,4 +13,21 @@ RSpec.describe Flight, type: :model do
     it {should have_many :flight_passengers}
     it {should have_many(:passengers).through(:flight_passengers)}
   end
+
+  describe "methods" do
+    it 'Can calculate adults with adults?' do
+      airline = Airline.create!(name: 'American Airlines')
+      flight = airline.flights.create!(number: "123", date: "01/26/2020", time: "2:00pm", departure_city: "Denver", arrival_city: "Dallas")
+      pass1 = flight.passengers.create!(name: 'Joe', age: 20)
+
+      expect(flight.adults?).to eq(1)
+    end
+    it 'Can calculate minors with minors?' do
+      airline = Airline.create!(name: 'American Airlines')
+      flight = airline.flights.create!(number: "123", date: "01/26/2020", time: "2:00pm", departure_city: "Denver", arrival_city: "Dallas")
+      pass2 = flight.passengers.create!(name: 'Jack', age: 17)
+
+      expect(flight.minors?).to eq(1)
+    end
+  end
 end
