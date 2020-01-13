3ad1892437905a13f1d1b56bfb98c685ee5fe642
DeMarcus Kirby <tkd024@gmail.com>
2020-01-13 10:42:29 -0700

    Add route for passenger show page

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get '/airlines', to: 'airlines#index'
   get '/airlines/:id', to: 'airlines#show'
   get '/flights/:id', to: 'flights#show'
+  get '/passengers/:id', to: "passengers#show"
 end
