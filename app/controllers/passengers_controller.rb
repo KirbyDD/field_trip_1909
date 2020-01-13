4524c0f210b81012223fc0edb842f3ec1a629883
DeMarcus Kirby <tkd024@gmail.com>
2020-01-13 11:21:42 -0700

    Add edit and update actions

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -2,4 +2,15 @@ class PassengersController < ApplicationController
   def show
     @passenger = Passenger.find(params[:id])
   end
+
+  def edit
+    @passenger = Passenger.find(params[:id])
+  end
+
+  def update
+    @passenger = Passenger.find(params[:id])
+    @flight = Flight.where(number: params[:number]).first
+    @flight.passengers << @passenger
+    redirect_to "/passengers/#{@passenger.id}"
+  end
 end
